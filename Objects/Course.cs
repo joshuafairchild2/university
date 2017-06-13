f280f5e9f5ecde3bb2e0e17538af65afcc98fd7d
Hunter Parks and Joshua Fairchild <hunter+joshua@>
2017-06-13 10:36:30 -0700

    fail then pass tests for methods that save a student to a course, as well as querying students of a given course

--- Objects/Course.cs
+++ Objects/Course.cs
@@ -42,7 +42,181 @@ namespace University.Objects
       _courseNumber = newCourseNumber;
     }
 
-    
-    //methods go here
+    public override bool Equals(System.Object otherCourse)
+    {
+      if(!(otherCourse is Course))
+      {
+        return false;
+      }
+      else
+      {
+        Course newCourse = (Course) otherCourse;
+        bool nameEquality = this.GetName() == newCourse.GetName();
+        bool idEquality = this.GetId() == newCourse.GetId();
+        bool numberEquality = this.GetCourseNumber() == newCourse.GetCourseNumber();
+        return (nameEquality && idEquality && numberEquality);
+      }
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("DELETE FROM courses;", conn);
+      cmd.ExecuteNonQuery();
+
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static List<Course> GetAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM courses", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      List<Course> courses = new List<Course>{};
+      while(rdr.Read())
+      {
+        int id = rdr.GetInt32(0);
+        string name = rdr.GetString(1);
+        string courseNumber = rdr.GetString(2);
+        Course newCourse = new Course(name, courseNumber, id);
+        courses.Add(newCourse);
+      }
+
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+
+      return courses;
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO courses (name, number) OUTPUT INSERTED.ID values (@CourseName, @CourseNumber)", conn);
+
+      SqlParameter nameParameter = new SqlParameter("@CourseName", this.GetName());
+      SqlParameter numberParameter = new SqlParameter("@CourseNumber", this.GetCourseNumber());
+
+      cmd.Parameters.Add(nameParameter);
+      cmd.Parameters.Add(numberParameter);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static Course Find(int idToFind)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM courses WHERE id = @CourseId", conn);
+      SqlParameter idParam = new SqlParameter("@CourseId", idToFind);
+      cmd.Parameters.Add(idParam);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      int id = 0;
+      string name = null;
+      string courseNumber = null;
+
+      while(rdr.Read())
+      {
+        id = rdr.GetInt32(0);
+        name = rdr.GetString(1);
+        courseNumber = rdr.GetString(2);
+      }
+
+      Course foundCourse  = new Course(name, courseNumber, id);
+
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+
+      return foundCourse;
+    }
+
+    public void AddStudent(Student studentToAdd)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO students_courses (student_id, course_id) VALUES (@StudentId, @CourseId);", conn);
+      SqlParameter studentIdParam = new SqlParameter("@StudentId", studentToAdd.GetId());
+      SqlParameter courseIdParam = new SqlParameter("@CourseId", this.GetId());
+      cmd.Parameters.Add(studentIdParam);
+      cmd.Parameters.Add(courseIdParam);
+
+      cmd.ExecuteNonQuery();
+
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public List<Student> GetStudents()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT students.* FROM courses JOIN students_courses ON (courses.id = students_courses.course_id) JOIN students ON (students.id = students_courses.student_id) WHERE courses.id = @CourseId;", conn);
+      SqlParameter courseIdParam = new SqlParameter("@CourseId", this.GetId());
+      cmd.Parameters.Add(courseIdParam);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      List<Student> students = new List<Student>{};
+      while(rdr.Read())
+      {
+        int id = rdr.GetInt32(0);
+        string name = rdr.GetString(1);
+        DateTime enrollDate = rdr.GetDateTime(2);
+        Student newStudent = new Student(name, enrollDate, id);
+        students.Add(newStudent);
+      }
+
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+
+      return students;
+    }
   }
 }
---
+++ Tests/CourseTest.cs
@@ -0,0 +1,78 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+
+namespace University.Objects
+{
+  [Collection("University")]
+  public class CourseTests : IDisposable
+  {
+    public CourseTests()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=university_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Course_GetAll_DatabaseEmptyOnload()
+    {
+      List<Course> testList = Course.GetAll();
+      List<Course> controlList = new List<Course>{};
+
+      Assert.Equal(controlList, testList);
+    }
+
+    [Fact]
+    public void Course_Save_SaveToDatabase()
+    {
+      Course newCourse = new Course("Computer Science", "CS101");
+      newCourse.Save();
+
+      Course testCourse = Course.GetAll()[0];
+      Assert.Equal(newCourse, testCourse);
+    }
+
+    [Fact]
+    public void Course_Equals_CourseEqualsCourse()
+    {
+      Course controlCourse = new Course("Computer Science", "CS101", 1);
+      Course testCourse = new Course("Computer Science", "CS101", 1);
+
+      Assert.Equal(controlCourse, testCourse);
+    }
+
+    [Fact]
+    public void Course_Find_FindsCourseInDB()
+    {
+      Course controlCourse = new Course("Computer Science", "CS101");
+      controlCourse.Save();
+
+      Course testCourse = Course.Find(controlCourse.GetId());
+
+      Assert.Equal(controlCourse, testCourse);
+    }
+
+    [Fact]
+    public void Course_AddStudent_AddsStudentToCourse()
+    {
+      Course newCourse = new Course("Computer Science", "CS101");
+      newCourse.Save();
+      Student newStudent1 = new Student("David", new DateTime(2015, 05, 12));
+      newStudent1.Save();
+      Student newStudent2 = new Student("John", new DateTime(2016, 05, 22));
+      newStudent2.Save();
+
+      newCourse.AddStudent(newStudent1);
+      newCourse.AddStudent(newStudent2);
+
+      List<Student> testList = newCourse.GetStudents();
+      List<Student> controlList = new List<Student>{newStudent1, newStudent2};
+
+      Assert.Equal(controlList, testList);
+    }
+
+    public void Dispose()
+    {
+      Course.DeleteAll();
+    }
+  }
+}
