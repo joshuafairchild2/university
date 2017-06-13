8481f45c3513743241016adb4e42da33643f1c32
Hunter Parks and Joshua Fairchild <hunter+joshua@>
2017-06-13 12:18:35 -0700

    create layout view, and check that app runs on kestrel server and is linking to stylesheets properly

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 using Nancy;
 using University.Objects;
+// using System.Data;
+// using System.Data.SqlClient;
 
 namespace University
 {
